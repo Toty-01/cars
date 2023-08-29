e1a6b43abd0fe578c829aaf209cc939dc7b09d9c
Toty-01 <caucheanthony@gmail.com>
2023-08-29 12:43:09 +0200

    premiers composants boutons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Rouler en toute liberté - Trouver le véhicule idéal
+        </h1>
+        <p className="hero__subtitle">
+          Commencer l'éxperience en ligne en parcourant notre parc de véhicules
+        </p>
+        <button className=''>
+          
+        </button>
+      </div>
+    </div>
   )
 }
 
