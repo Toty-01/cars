343f556490d595d35ceef92d2dfbde075ba6640a
Toty-01 <caucheanthony@gmail.com>
2023-11-05 19:06:37 +0100

    rendu des voitures filtres

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ export async function fetchCars() {
 		'X-RapidAPI-Key': 'c7c33def0fmsh80f303f2042bca6p1b411ajsnb6405434b509',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
     headers: headers,
   });
   const result = await response.json();
