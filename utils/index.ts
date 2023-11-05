cdcbe72d9ae7ae5ca534002be3b55fb3d793dae3
Toty-01 <caucheanthony@gmail.com>
2023-11-05 18:33:03 +0100

    filtre voiture + api connexion

---
+++ utils/index.ts
@@ -0,0 +1,11 @@
+export async function fetchCars() {
+  const headers = {	
+		'X-RapidAPI-Key': 'c7c33def0fmsh80f303f2042bca6p1b411ajsnb6405434b509',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+    headers: headers,
+  });
+  const result = await response.json();
+  return result;
+}
